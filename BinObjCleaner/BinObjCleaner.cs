30a1b7d3469a873d9859bb509b9b49074fede233
gparlakov <gparlakov@gmail.com>
2013-05-07 11:08:30 -0700

    First add two applications. Tested and working

---
+++ BinObjCleaner/BinObjCleaner.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace BinObjCleaner
+{
+    class BinObjCleaner
+    {
+        static void Main()
+        {
+            try
+            {
+                var currentDirectory = Directory.GetCurrentDirectory();
+                var binDirectories = Directory.GetDirectories(currentDirectory, "bin", SearchOption.AllDirectories);
+                foreach (var subDirectory in binDirectories)
+                {
+                    Directory.Delete(subDirectory, true);
+                }
+
+                var objDirectories = Directory.GetDirectories(currentDirectory, "obj", SearchOption.AllDirectories);
+                foreach (var dir in objDirectories)
+                {
+                    Directory.Delete(dir, true);
+                }
+            }
+            catch (Exception ex)
+            {
+                foreach (var key in ex.Data.Keys)
+                {
+                    Console.WriteLine(ex.Data[key]);
+                }
+            }
+        }
+    }
+}
---
+++ VS12ToVs10Converter/VS12ToVs10Converter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace VS12ToVs10Converter
+{
+    class VS12ToVs10Converter
+    {
+        static void Main()
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var files = Directory.EnumerateFiles(currentDirectory, "*.csproj", SearchOption.AllDirectories);
+                       
+            foreach (var file in files)
+            {
+                string fileContent = ReadFile(file);
+                string chnagedContent = fileContent.Replace(
+                    "<TargetFrameworkVersion>v4.5</TargetFrameworkVersion>", 
+                    "<TargetFrameworkVersion>v4.0</TargetFrameworkVersion>");
+
+                OverwriteFile(file, chnagedContent);
+            }
+        }
+
+        private static void OverwriteFile(string file, string content)
+        {
+            StreamWriter writer = new StreamWriter(file);
+            using (writer)
+            {
+                writer.Write(content);
+            }
+        }
+
+        private static string ReadFile(string file)
+        {
+            string fileContent;
+            StreamReader reader = new StreamReader(file);
+            using (reader)
+            {
+                fileContent = reader.ReadToEnd();
+            }
+
+            return fileContent;
+        }
+    }
+}
